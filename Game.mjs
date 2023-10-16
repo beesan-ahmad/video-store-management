bdddc336152b778d5719859272d1421526823cbc
MohammadNR <tntpsnr@gmail.com>
2023-10-16 15:11:01 +0300

    Make a dynamic id and export game class

--- Game.mjs
+++ Game.mjs
@@ -1,4 +1,4 @@
-class Game {
+export class Game {
     id;
     gameName;
     description;
@@ -9,6 +9,7 @@ class Game {
     requirments;
     discountPercentage;
     constructor(gameName, description, publicherCompany, category, price, gameState, requirments, discountPercentage) {
+        this.id = Math.floor(new Date().valueOf() * Math.random());
         this.gameName = gameName;
         this.description = description;
         this.publicherCompany = publicherCompany;
