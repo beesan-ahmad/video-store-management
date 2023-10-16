6237019f6b0f19aec39837ef4638f548fb99a1cd
beesan-ahmad <abumfarreh@gmail.com>
2023-10-16 12:18:10 +0300

    create a list of categories to add a new category to the list

--- Category.mjs
+++ Category.mjs
@@ -1,14 +1,9 @@
 export class Category {
     id;
-    name;
-    constructor(id, name) {
-        this.id = id.Math.floor(new Date().valueOf() * Math.random());
-        this.name = name;
+    categoryName;
+    constructor(categoryName) {
+        this.id = Math.floor(new Date().valueOf() * Math.random());
+        this.categoryName = categoryName;
     }
-    addCategory(value) {
-        if (!value || !typeof value == "string") {
-            let categoryObj = new Category();
-            categoryObj.addCategory(value);
-        }
-    }
+
 }
