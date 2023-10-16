765767030413acf9b5996815749653b9265fba57
MohammadNR <tntpsnr@gmail.com>
2023-10-16 11:44:24 +0300

    change publisher class  structure

--- Publisher.mjs
+++ Publisher.mjs
@@ -1,16 +1,8 @@
 export class Publisher {
     id;
     publisherName;
-    constructor () {
+    constructor (publisherName) {
         this.id = Math.floor(new Date().valueOf() * Math.random());
-    }
-
-    addPublisher(publisherName) {
-        if (publisherName && typeof publisherName === 'string') {
-            this.publisherName = publisherName;
-            console.log("add done.");
-        } else {
-            console.log("please check name is not valid!");
-        }
+        this.publisherName=publisherName;
     }
 }
