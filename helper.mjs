df867dd62403f580f65223b1e71fdd0085c1c25c
MohammadNR <tntpsnr@gmail.com>
2023-10-17 14:28:35 +0300

    follow clean code conventions

--- helper.mjs
+++ helper.mjs
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const validationFunction = (vaule, expectedType, variableName) => {
-    if (typeof vaule === expectedType && vaule  && (expectedType === 'number'?(vaule >= 0?true:false):true)) {
+export const validationFunction = (value, expectedType, variableName) => {
+    if (typeof value === expectedType && value  && (expectedType === 'number'?(value >= 0?true:false):true)) {
         return true;
     } else {
         console.log(`pleace enter valid ${variableName} with type :${expectedType} `);
