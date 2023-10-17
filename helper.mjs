67b4d5ea6cc6dcfc0faa4c5a5acf5dd2cdee7db6
MohammadNR <tntpsnr@gmail.com>
2023-10-17 10:02:32 +0300

    Add greadlineSync library and create getInput method to handle user input

--- helper.mjs
+++ helper.mjs
@@ -1,3 +1,5 @@
+import readlineSync from 'readline-sync';
+
 export const validationFunction = (vaule, expectedType, variableName) => {
     if (typeof vaule === expectedType && vaule  && (expectedType === 'number'?(vaule >= 0?true:false):true)) {
         return true;
@@ -5,5 +7,8 @@ export const validationFunction = (vaule, expectedType, variableName) => {
         console.log(`pleace enter valid ${variableName} with type :${expectedType} `);
         return false;
     }
-
 }
+
+export const  getInput = (input) => {
+    return readlineSync.question(input);
+  }
