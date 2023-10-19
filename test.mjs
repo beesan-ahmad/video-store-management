9123666c56287c43c02ed12529d768b620b4aaea
MohammadNR <tntpsnr@gmail.com>
2023-10-19 18:34:42 +0300

    Add test file and case

---
+++ test.mjs
@@ -0,0 +1,32 @@
+import { Category } from "./Category.mjs";
+import { CategoryService } from "./CategoryService.mjs";
+import { GameService } from "./GameService.mjs";
+import { Publisher } from "./Publisher.mjs";
+import { PublisherService } from "./PublisherService.mjs";
+import { Interface } from "./interface.mjs";
+
+const gameService = new GameService();
+const interfaceObj = new Interface();
+
+for (let i = 1; i <= 10; i++) {
+    PublisherService.addPublisher(new Publisher(`Publisher ${i}`));
+}
+
+// Add 10 categories
+for (let i = 1; i <= 10; i++) {
+    CategoryService.addCategory(new Category(`Category ${i}`));
+}
+
+for (let i = 1; i <= 10; i++) {
+    interfaceObj.gameService.addNewGame(
+        `Game ${i}`,
+        `Description of Game ${i}`,
+        new Publisher("amg"),
+        new Category("action"),
+        50, // Set your desired price here
+        'paid', // Set the game state accordingly
+        "Requirements for Game ${i}",
+        10 // Set the discount percentage here
+    );
+}
+interfaceObj.start();
